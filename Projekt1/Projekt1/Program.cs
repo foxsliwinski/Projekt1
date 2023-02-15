743fd9db0cc66bddc3f5653a27f01cccae2173b7
Chuwi <Chuwi@WIN-19L8O9AO8L7>
2023-02-15 17:39:26 +0100

    zadanie dzień 4

--- Projekt1/Projekt1/Program.cs
+++ Projekt1/Projekt1/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, Andrzej");
+﻿// zadanie domowe dzień 4
+using System.Text.Json.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+
+string name = "ewa";
+bool female = false;
+var age = 33;
+
+if (female && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+
+else if (age == 33 && name == "ewa")
+{
+    Console.WriteLine("ewa, lat 33");
+}
+
+else if (!female && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+
+}
